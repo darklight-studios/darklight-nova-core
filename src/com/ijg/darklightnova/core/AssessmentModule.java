169b9b7236b58c20019896dbb2793f0e2816e87b
Lucas Nicodemus <shank@shanked.me>
2012-09-25 01:16:48 -0600

    More sanity checks if there was no net connection when user entered name

--- src/com/ijg/darklightnova/core/AssessmentModule.java
+++ src/com/ijg/darklightnova/core/AssessmentModule.java
@@ -51,8 +51,13 @@ public class AssessmentModule {
 			engine.writeFoundList();
 		}
 		
+		//If somehow you broke the engine when boot happened and had no network, we can still create an account for you regardless of your faulty connection at first.
+		if (!engine.database.userExists(engine.getUserName(), engine.sessionid)) {
+			engine.database.insertNewUser(engine.getUserName(), engine.sessionid);
+		}
 		//Regardless of if it changed or not, send an update. This way, a loss in internet connection won't mean you're locked out until your score changes.
 		engine.database.updateUserScore(engine.getUserName(), engine.sessionid, issues.size());
+		
 	}
 	
 	public String toString() {
