55257ebedcee2fdb9828981af4bd7a9cef92e434
Isaac Grant <waterwolf2.0@gmail.com>
2012-11-20 22:11:31 -0700

    improved readability

--- src/com/ijg/darklight/core/settings/Settings.java
+++ src/com/ijg/darklight/core/settings/Settings.java
@@ -1,16 +1,16 @@
 package com.ijg.darklight.core.settings;
 
 public enum Settings {
-	PROGRESS_FILE(Parser.getValue("general", "progress_file")),
-	ID_VERIFICATION(Parser.getValue("general", "idverification")),
-	SESSION_TYPE(Parser.getValue("general", "session_type")),
-	API_ID(Parser.getValue("api", "id")),
-	NAME_FILE(Parser.getValue("api", "name_file")),
-	API_PROTOCOL(Parser.getValue("api", "protocol")),
-	API_SERVER(Parser.getValue("api", "server")),
-	VERIFICATION_ACTIVE(Parser.getValue("verification", "active")),
-	VERIFICATION_NAMES(Parser.getValue("verification", "names")),
-	VERIFICATION_TEAMS(Parser.getValue("verification", "teams"));
+	PROGRESS_FILE        (Parser.getValue("general", "progress_file")),
+	ID_VERIFICATION      (Parser.getValue("general", "idverification")),
+	SESSION_TYPE         (Parser.getValue("general", "session_type")),
+	API_ID               (Parser.getValue("api", "id")),
+	NAME_FILE            (Parser.getValue("api", "name_file")),
+	API_PROTOCOL         (Parser.getValue("api", "protocol")),
+	API_SERVER           (Parser.getValue("api", "server")),
+	VERIFICATION_ACTIVE  (Parser.getValue("verification", "active")),
+	VERIFICATION_NAMES   (Parser.getValue("verification", "names")),
+	VERIFICATION_TEAMS   (Parser.getValue("verification", "teams"));
 	
 	private String value;
 	
