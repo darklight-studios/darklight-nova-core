86d57603972f3ac583f5273c5dd98e0635bc1f55
Isaac Grant <waterwolf2.0@gmail.com>
2012-11-25 20:31:48 -0700

    optimized settings access

--- src/com/ijg/darklight/core/settings/Settings.java
+++ src/com/ijg/darklight/core/settings/Settings.java
@@ -1,8 +1,9 @@
 package com.ijg.darklight.core.settings;
 
+import org.json.simple.JSONArray;
+
 public enum Settings {
 	PROGRESS_FILE        (Parser.getValue("general", "progress")),
-	ID_VERIFICATION      (Parser.getValue("general", "idverification")),
 	SESSION_TYPE         (Parser.getValue("general", "sessiontype")),
 	API_ID               (Parser.getValue("api", "id")),
 	NAME_FILE            (Parser.getValue("api", "name")),
@@ -18,7 +19,55 @@ public enum Settings {
 		this.value = value;
 	}
 	
-	public Object value() {
+	private Object value() {
 		return value;
 	}
+	
+	public static String get(String setting) {
+		switch (setting) {
+			case "progressfile":
+				return (String) PROGRESS_FILE.value();
+			case "sessiontype":
+				return (String) SESSION_TYPE.value();
+			case "api.id":
+				return String.valueOf((Long) API_ID.value());
+			case "namefile":
+				return (String) NAME_FILE.value();
+			case "api.protocol":
+				return (String) API_PROTOCOL.value();
+			case "api.server":
+				return (String) API_SERVER.value();
+			default:
+				System.out.println("Error, regular setting does not exist: " + setting);
+				break;
+		}
+		return "";
+	}
+	
+	public static boolean getBool(String setting) {
+		switch (setting) {
+			case "verification.active":
+				return (((String) VERIFICATION_ACTIVE.value()) == "true") ? true : false;
+			default:
+				System.out.println("Error, boolean setting does not exist: "
+						+ setting);
+				break;
+		}
+		return false;
+	}
+	
+	public static JSONArray getJSON(String setting) {
+		switch (setting) {
+			case "verification.names":
+				return (JSONArray) VERIFICATION_NAMES.value();
+			case "verification.teams":
+				return (JSONArray) VERIFICATION_TEAMS.value();
+			default:
+				System.out
+						.println("JSON setting does not exist: " + setting);
+				break;
+		}
+		return null;
+	}
+	
 }
