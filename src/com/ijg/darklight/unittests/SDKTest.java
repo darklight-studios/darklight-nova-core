c4b4238a2f9850251d6b0db94edb31b3670703e8
Isaac Grant <waterwolf2.0@gmail.com>
2012-11-26 10:05:26 -0700

    initial commit

---
+++ src/com/ijg/darklight/unittests/SDKTest.java
@@ -0,0 +1,21 @@
+package com.ijg.darklight.unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.ijg.darklight.core.settings.Settings;
+import com.ijg.darklight.web.sdk.DarklightSDK;
+
+public class SDKTest {
+
+	@Test
+	public void test() {
+		DarklightSDK sdk = new DarklightSDK(Settings.get("api.protocol"), Settings.get("api.server"), Long.parseLong(Settings.get("api.id")));
+		
+		assertNotNull(sdk.api());
+		
+		
+	}
+
+}
