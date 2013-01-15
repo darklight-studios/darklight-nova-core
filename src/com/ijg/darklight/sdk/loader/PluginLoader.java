4df80a3e235126c5e48f0e605ce0cfaf87134c61
Isaac Grant <waterwolf2.0@gmail.com>
2013-01-14 19:51:08 -0700

    changed package names to load

--- src/com/ijg/darklight/sdk/loader/PluginLoader.java
+++ src/com/ijg/darklight/sdk/loader/PluginLoader.java
@@ -25,7 +25,7 @@ public class PluginLoader {
 					String name = plugin.getName().substring(0, plugin.getName().indexOf("."));
 					System.out.println("Loading plugin: " + name + "...");
 					try {
-						plugins.add((Plugin) DarklightLoader.loadAndInstantiateJar("com.ijg.darklight.core.plugins." + name, plugin.getPath()));
+						plugins.add((Plugin) DarklightLoader.loadAndInstantiateJar("com.darklight.core.plugins." + name, plugin.getPath()));
 					} catch (InstantiationException | IllegalAccessException
 							| ClassNotFoundException e) {
 						System.err.println("Error loading module \"" + name
@@ -55,7 +55,7 @@ public class PluginLoader {
 						String name = module.getName().substring(0, module.getName().indexOf("."));
 						System.out.println("Loading module: " + name + "...");
 						try {
-							modules.add((ScoreModule) DarklightLoader.loadAndInstantiateJar("com.ijg.darklight.core.scoring." + name, module.getPath()));
+							modules.add((ScoreModule) DarklightLoader.loadAndInstantiateJar("com.darklight.core.scoring." + name, module.getPath()));
 						} catch (InstantiationException
 								| IllegalAccessException
 								| ClassNotFoundException e) {
