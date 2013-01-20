18dee5cd8f99f49d40b34935aa4c437373332091
Isaac Grant <waterwolf2.0@gmail.com>
2013-01-20 11:34:38 -0700

    documented

--- src/com/ijg/darklight/sdk/core/AccessHandler.java
+++ src/com/ijg/darklight/sdk/core/AccessHandler.java
@@ -5,14 +5,26 @@ import java.util.ArrayList;
 public class AccessHandler {
 	private CoreEngine engine;
 	
+	/**
+	 * 
+	 * @param engine The instance of CoreEngine this AccessHandler will use
+	 */
 	public AccessHandler(CoreEngine engine) {
 		this.engine = engine;
 	}
 	
+	/**
+	 * Get the instance of Settings currently used by the CoreEngine
+	 * @return The instance of Settings currently being used
+	 */
 	public Settings getSettings() {
 		return engine.settings;
 	}
 	
+	/**
+	 * Get an ArrayList of fixed issues
+	 * @return An ArrayList of fixed issues
+	 */
 	public ArrayList<Issue> getFixedIssues() {
 		return engine.moduleHandler.getIssues();
 	}
