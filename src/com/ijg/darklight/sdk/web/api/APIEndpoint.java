b7e88d646be79be8d19bc9fc14d515f260d0020a
Isaac Grant <waterwolf2.0@gmail.com>
2013-01-21 17:12:14 -0700

    fixed endpoint endings (removed trailing slashes)

--- src/com/ijg/darklight/sdk/web/api/APIEndpoint.java
+++ src/com/ijg/darklight/sdk/web/api/APIEndpoint.java
@@ -3,8 +3,8 @@ package com.ijg.darklight.sdk.web.api;
 public enum APIEndpoint {
 	AUTH		("/api/auth"),
 	UPDATE		("/api/update"),
-	INDIVIDUAL 	("/session/individual/"),
-	TEAM		("/session/team/");
+	INDIVIDUAL 	("/session/individual"),
+	TEAM		("/session/team");
 	
 	private String endpoint;
 	
