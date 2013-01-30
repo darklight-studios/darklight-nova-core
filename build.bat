388a6a9df5a2421b9189bc029ca28459bfde407e
Isaac Grant <waterwolf2.0@gmail.com>
2013-01-29 19:49:04 -0700

    renamed output jar to DarklightSDK, and now builds a seperate javadoc jar

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
 javac -sourcepath src -cp res/gson-2.2.2.jar -d bin src\com\ijg\darklight\sdk\core\CoreEngine.java
-jar cvfm Darklight.jar Manifest.MF -C bin/ .
+jar cvfm DarklightSDK.jar Manifest.MF -C bin/ .
+jar cvf DarklightSDK-javadoc.jar -C doc/ .
+javac -sourcepath src -cp res/gson-2.2.2.jar -d bin src\com\ijg\darklight\build\DarklightInstaller.java
+jar cvfm DarklightInstaller.jar InstallerManifest.MF -C bin/ .
