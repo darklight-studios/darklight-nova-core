0440bb6fd11b211cb54ea08ace8c16644b0bf895
Isaac Grant <waterwolf2.0@gmail.com>
2013-02-07 18:09:42 -0700

    packages statics and templates directory into installer jar

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ javac -sourcepath src -cp res/gson-2.2.2.jar -d bin src\com\ijg\darklight\sdk\co
 jar cvfm DarklightSDK.jar Manifest.MF -C bin/ .
 jar cvf DarklightSDK-javadoc.jar -C doc/ .
 javac -sourcepath src -cp res/gson-2.2.2.jar -d bin src\com\ijg\darklight\build\DarklightInstaller.java
-jar cvfm DarklightInstaller.jar InstallerManifest.MF -C bin/ .
+jar cvfm DarklightInstaller.jar InstallerManifest.MF -C bin/ . statics/ templates/
