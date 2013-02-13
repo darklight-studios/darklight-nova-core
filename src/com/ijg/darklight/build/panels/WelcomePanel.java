5d042660c24da8bf4843f8c65b375e620009b95b
Isaac Grant <waterwolf2.0@gmail.com>
2013-02-12 18:21:01 -0700

    added copyright to top of all source files

--- src/com/ijg/darklight/build/panels/WelcomePanel.java
+++ src/com/ijg/darklight/build/panels/WelcomePanel.java
@@ -10,6 +10,25 @@ import javax.swing.JPanel;
 import com.ijg.darklight.build.InstallerFrame;
 import com.ijg.darklight.build.Utils;
 
+/*
+ * Copyright (C) 2013  Isaac Grant
+ * 
+ * This file is part of the Darklight Nova Core.
+ *  
+ * Darklight Nova Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Darklight Nova Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Darklight Nova Core.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class WelcomePanel extends JPanel {
 	private static final long serialVersionUID = -1411206033690589925L;
 	
