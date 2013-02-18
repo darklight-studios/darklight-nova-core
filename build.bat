1f7b128e9c5d1017fc8de445d6d450e50c906488
Isaac Grant <waterwolf2.0@gmail.com>
2013-02-18 11:46:14 -0700

    compile utils

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 javac -sourcepath src -cp res/gson-2.2.2.jar -d bin src\com\ijg\darklight\sdk\core\CoreEngine.java
+javac -d bin src\com\ijg\darklight\sdk\utils\*
 jar cvfm DarklightSDK.jar Manifest.MF -C bin/ .
 jar cvf DarklightSDK-javadoc.jar -C doc/ .
 javac -sourcepath src -cp res/gson-2.2.2.jar -d bin src\com\ijg\darklight\build\DarklightInstaller.java
