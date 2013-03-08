5b6a43081fa5955a097cb6f5a1405b7bb505595e
Isaac Grant <waterwolf2.0@gmail.com>
2013-03-07 18:56:22 -0700

    package gson jar with the installer

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ javac -d bin src\com\ijg\darklight\sdk\utils\*
 jar cvfm DarklightSDK.jar Manifest.MF -C bin/ .
 jar cvf DarklightSDK-javadoc.jar -C doc/ .
 javac -sourcepath src -cp res/gson-2.2.2.jar -d bin src\com\ijg\darklight\build\DarklightInstaller.java
-jar cvfm DarklightInstaller.jar InstallerManifest.MF -C bin/ . statics/ templates/
+jar cvfm DarklightInstaller.jar InstallerManifest.MF -C bin/ . statics/ templates/ res/gson-2.2.2.jar
